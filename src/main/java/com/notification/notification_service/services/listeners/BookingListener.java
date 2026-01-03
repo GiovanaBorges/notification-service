74925b7884b6e47171280fde4fb5502a694fa8eb
giovana borges <gigi12345as17@gmail.com>
2026-01-03 15:03:15 -0300

    feat: config listener for queue created updated deleted

---
+++ src/main/java/com/notification/notification_service/services/listeners/BookingListener.java
@@ -0,0 +1,31 @@
+package com.notification.notification_service.services.listeners;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import com.notification.notification_service.DTO.NotificationEventDTO;
+
+@Service
+public class BookingListener {
+     private final SimpMessagingTemplate messaging;
+
+    public BookingListener(SimpMessagingTemplate messaging) {
+        this.messaging = messaging;
+    }
+
+    @RabbitListener(queues = "${rabbitmq.bookings.queue.created}")
+    public void onBookingCreateEvent(NotificationEventDTO dto) {
+        messaging.convertAndSend("/topic/bookings/created", dto);
+    }
+
+    @RabbitListener(queues = "${rabbitmq.bookings.queue.updated}")
+    public void onBookingUpdateEvent(NotificationEventDTO dto) {
+        messaging.convertAndSend("/topic/bookings/updated", dto);
+    }
+
+    @RabbitListener(queues = "${rabbitmq.bookings.queue.deleted}")
+    public void onBookingDeleteEvent(NotificationEventDTO dto) {
+        messaging.convertAndSend("/topic/bookings/deleted", dto);
+    }
+}
